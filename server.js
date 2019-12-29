8fa598a8d4826dda12066814d655a7cddcbd8ca4
Paul Muchiri <58714292+pmuchiri@users.noreply.github.com>
2019-12-29 14:36:00 -0500

    codepm-trade
    trade platform practice using JS and NodeJS

---
+++ server.js
@@ -0,0 +1,21 @@
+// ./server.js
+const stockData = require('./stock.json');
+const Pusher = require('pusher');
+
+
+var pusher = new Pusher({
+  appId: 'APP_ID',
+  key: 'APP_KEY',
+  secret: 'APP_SECRET',
+  cluster: 'APP_CLUSTER',
+  encrypted: true
+});
+
+// ./server.js
+let i = 0;
+setInterval(() => {
+  const GOOG = stockData[1]['Trades'][i];
+  pusher.trigger('trade', 'stock', GOOG);
+  i++;
+}, 2000);
+
